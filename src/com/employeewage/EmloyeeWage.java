fbfd2b52d257f6f6ac450923291e7808b89cc8f6
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-12 13:01:12 +0530

    Added: Added Switch case to the code

--- src/com/employeewage/EmloyeeWage.java
+++ src/com/employeewage/EmloyeeWage.java
@@ -6,29 +6,28 @@ public class EmloyeeWage {
 	{
 		System.out.println("Welcome to Employee Wage Builder");
 		Random rand = new Random();
-		int attendance=rand.nextInt(2);
-		int workinghr,WAGEPERHR=20;
+		int attendance=rand.nextInt(3);
+			
+		int workinghr=0,WAGEPERHR=20;
 		
-		if (attendance==0)
+		switch(attendance)
 		{
-			System.out.println("Employee is Absent");
-			workinghr=0;
+			case 0:
+					System.out.println("Employee is Absent");
+					workinghr=0;
+					break;
+			case 1:
+				 	System.out.println("Employee is Present");
+					System.out.println("Employee Is Working For Half Day");
+					workinghr=4;
+					break;
+			case 2:
+					System.out.println("Employee is Present");
+					System.out.println("Employee Is Working For Full  Day");
+					workinghr=8;
 		}
 		
-		else
-		{
-			System.out.println("Employee is Present");
-			int checkworkTm=rand.nextInt(2);
-			if (checkworkTm==0)
-			{	System.out.println("Employee Is Working For Half Day");
-				workinghr=4;
-			}
-			else
-			{	System.out.println("Employee Is Working For Full  Day");
-				workinghr=8;
-			}
-		}
+		System.out.println("Employee Earn : " + workinghr*WAGEPERHR );
 		
-		System.out.println("Employee Earn :" + workinghr*WAGEPERHR );
 	}
 }
