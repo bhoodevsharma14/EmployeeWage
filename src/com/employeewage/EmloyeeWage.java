4bed375955bde338f33f73443004dbd3fcf10c84
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-12 09:34:23 +0530

    Checking Employee is Present or Absent

--- src/com/employeewage/EmloyeeWage.java
+++ src/com/employeewage/EmloyeeWage.java
@@ -1,9 +1,15 @@
 package com.employeewage;
-
+import java.util.Random;
 public class EmloyeeWage {
 
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Builder");
+		Random rand = new Random();
+		int attendance=rand.nextInt(2);
+		if (attendance==0)
+			System.out.println("Employee is Absent");
+		else
+			System.out.println("Employee is Present");
 	}
 }
