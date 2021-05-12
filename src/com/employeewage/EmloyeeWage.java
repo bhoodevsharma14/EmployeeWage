2e5b6c9321a27052dd8aa799619d64e2d499262a
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-12 10:59:08 +0530

    Added: Single Day Wage is calculated

--- src/com/employeewage/EmloyeeWage.java
+++ src/com/employeewage/EmloyeeWage.java
@@ -7,9 +7,20 @@ public class EmloyeeWage {
 		System.out.println("Welcome to Employee Wage Builder");
 		Random rand = new Random();
 		int attendance=rand.nextInt(2);
+		int workinghr,WAGEPERHR=20;
+		
 		if (attendance==0)
+		{
 			System.out.println("Employee is Absent");
+			workinghr=8;
+		}
+		
 		else
+		{
 			System.out.println("Employee is Present");
+			workinghr=0;
+		}
+		
+		System.out.println("Employee Earn :" + workinghr*WAGEPERHR );
 	}
 }
